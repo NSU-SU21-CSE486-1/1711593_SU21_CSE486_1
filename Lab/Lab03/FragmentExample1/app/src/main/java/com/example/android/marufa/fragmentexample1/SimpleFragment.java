58ca94245180d498137c245cf90afce267fb003f
fahemasultana <56092087+fahemasultana@users.noreply.github.com>
2021-07-16 02:52:32 +0600

    #3 finish code challange for lab 3

--- Lab/Lab03/FragmentExample1/app/src/main/java/com/example/android/marufa/fragmentexample1/SimpleFragment.java
+++ Lab/Lab03/FragmentExample1/app/src/main/java/com/example/android/marufa/fragmentexample1/SimpleFragment.java
@@ -8,7 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -68,6 +70,7 @@ public class SimpleFragment extends Fragment {
         final View rootView =
                 inflater.inflate(R.layout.fragment_simple, container, false);
         final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+        final RatingBar ratingBar = rootView.findViewById(R.id.ratingBar);
 
 // TODO: Set the radioGroup onCheckedChanged listener.
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -90,6 +93,16 @@ public class SimpleFragment extends Fragment {
                 }
             }
         });
+
+
+        ratingBar.setOnRatingBarChangeListener
+                (new RatingBar.OnRatingBarChangeListener() {
+                     @Override
+                     public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                         String myRating = (getString(R.string.my_rating) + String.valueOf(ratingBar.getRating()));
+                         Toast.makeText(getContext(), myRating, Toast.LENGTH_SHORT).show();
+                     }
+                 });
 // Return the View for the fragment's UI.
         return rootView;
     }
