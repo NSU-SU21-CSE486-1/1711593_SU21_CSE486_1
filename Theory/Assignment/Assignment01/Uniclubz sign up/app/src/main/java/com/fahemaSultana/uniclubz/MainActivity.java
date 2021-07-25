1f7e79ef7c782695758aab109899f0896cd273d9
fahemasultana <56092087+fahemasultana@users.noreply.github.com>
2021-07-25 23:41:53 +0600

    #4 update manifest and second activity

--- Theory/Assignment/Assignment01/Uniclubz sign up/app/src/main/java/com/fahemaSultana/uniclubz/MainActivity.java
+++ Theory/Assignment/Assignment01/Uniclubz sign up/app/src/main/java/com/fahemaSultana/uniclubz/MainActivity.java
@@ -1,14 +1,36 @@
 package com.fahemaSultana.uniclubz;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "com.fahemaSultana.uniclubz";
+    private EditText enter_name_Sign;
+    private EditText enter_email_Sign;
+    private EditText enter_password_Sign;
+    private EditText enter_confirm_password_Sign;
+    public static final int TEXT_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        enter_name_Sign = findViewById(R.id.enter_name_signup);
+        enter_email_Sign = findViewById(R.id.enter_email_signup);
+        enter_password_Sign = findViewById(R.id.enter_password_signup);
+        enter_confirm_password_Sign = findViewById(R.id.enter_confirm_pass_signup);
+    }
+
+    public void profileinfo(View view) {
+        Intent profile = new Intent(this, Student_Profile.class);
+        String Name = enter_name_Sign.getText().toString();
+        profile.putExtra(EXTRA_MESSAGE, Name);
+        startActivity(profile);
     }
 }
--- Theory/Assignment/Assignment01/Uniclubz sign up/app/src/main/java/com/fahemaSultana/uniclubz/Student_Profile.java
+++ Theory/Assignment/Assignment01/Uniclubz sign up/app/src/main/java/com/fahemaSultana/uniclubz/Student_Profile.java
@@ -2,7 +2,9 @@ package com.fahemaSultana.uniclubz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Student_Profile extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Student_Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student__profile);
+        Intent intent = getIntent();
+        String name_sign = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.show_profile_name);
+        textView.setText(name_sign);
     }
+
+
 }
