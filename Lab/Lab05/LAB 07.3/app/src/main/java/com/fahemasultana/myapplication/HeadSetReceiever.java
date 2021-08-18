bef6411692cdee908ff94c73c4aa1e962c5d9ecd
fahemasultana <56092087+fahemasultana@users.noreply.github.com>
2021-08-19 03:57:08 +0600

    #32 show tost

--- Lab/Lab05/LAB 07.3/app/src/main/java/com/fahemasultana/myapplication/HeadSetReceiever.java
+++ Lab/Lab05/LAB 07.3/app/src/main/java/com/fahemasultana/myapplication/HeadSetReceiever.java
@@ -3,13 +3,21 @@ package com.fahemasultana.myapplication;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
+
+import static android.widget.Toast.*;
 
 public class HeadSetReceiever extends BroadcastReceiver {
+    String toastmsg = "conected";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
+        String intentAction = intent.getAction();
         throw new UnsupportedOperationException("Not yet implemented");
+
+
+        if (intentAction != null) {
+            Toast.makeText(context, toastmsg, LENGTH_SHORT).show();
+        }
     }
 }
--- Lab/Lab05/LAB 07.3/app/src/main/java/com/fahemasultana/myapplication/MainActivity.java
+++ Lab/Lab05/LAB 07.3/app/src/main/java/com/fahemasultana/myapplication/MainActivity.java
@@ -2,13 +2,28 @@ package com.fahemasultana.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private HeadSetReceiever mReceiver = new HeadSetReceiever();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter();
+
+
+        filter.addAction(Intent.ACTION_HEADSET_PLUG);
+        this.registerReceiver(mReceiver, filter);
+
+
+
+
+
     }
 }
