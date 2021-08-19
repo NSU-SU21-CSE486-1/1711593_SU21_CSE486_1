25352dec9172f96cef69939b4997a7a11f159399
fahemasultana <56092087+fahemasultana@users.noreply.github.com>
2021-08-19 18:10:47 +0600

    #32 Lab work 5 done

--- Lab/Lab05/LAB 07.3/app/src/main/java/com/fahemasultana/myapplication/HeadSetReceiever.java
+++ Lab/Lab05/LAB 07.3/app/src/main/java/com/fahemasultana/myapplication/HeadSetReceiever.java
@@ -3,21 +3,42 @@ package com.fahemasultana.myapplication;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
-import static android.widget.Toast.*;
+import javax.security.auth.Destroyable;
 
 public class HeadSetReceiever extends BroadcastReceiver {
     String toastmsg = "conected";
+    private Object Tag;
+    private String TAG = "HeadSet";
+    private String toastMessage;
+
+    public HeadSetReceiever() {
+        Log.d(TAG, "Created");
+
+    }
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
         String intentAction = intent.getAction();
-        throw new UnsupportedOperationException("Not yet implemented");
-
-
         if (intentAction != null) {
-            Toast.makeText(context, toastmsg, LENGTH_SHORT).show();
+
+            switch (intentAction) {
+                case Intent.ACTION_HEADSET_PLUG:
+                    toastMessage = "HeadSet connected!";
+                    break;
+                default:
+                    toastMessage = "HeadSet not Connected";
+                    break;
+            }
+
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
         }
     }
+
+
+
 }
+
--- Lab/Lab05/LAB 07.3/app/src/main/java/com/fahemasultana/myapplication/MainActivity.java
+++ Lab/Lab05/LAB 07.3/app/src/main/java/com/fahemasultana/myapplication/MainActivity.java
@@ -1,23 +1,27 @@
 package com.fahemasultana.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private HeadSetReceiever mReceiver = new HeadSetReceiever();
+    private static final String TAG = "MainActivity";
+    private HeadSetReceiever mReceiver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mReceiver = new HeadSetReceiever();
+
         IntentFilter filter = new IntentFilter();
-
-
         filter.addAction(Intent.ACTION_HEADSET_PLUG);
         this.registerReceiver(mReceiver, filter);
 
@@ -25,5 +29,17 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Unregister the receivers.
+        this.unregisterReceiver(mReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 }
+
+
